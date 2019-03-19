88654fdcd2462379b2bdf8808136e0dc88dff936
PatrickSorensen <11074388+PatrickSorensen@users.noreply.github.com>
2019-03-19 11:49:19 +0100

    Update simplePackageFilter.cs
    IPv4 packet splitting

--- simplePackageFilter.cs
+++ simplePackageFilter.cs
@@ -274,6 +274,24 @@ namespace simplePackageFilter
                   .BuildCSVFile()
                   .BuildVHDL();
 
+                byte[] arr = { 0, 48, 136, 21, 69, 131, 0, 24, 231, 253, 174, 161, 8, 0, 69, 0, 0, 52, 2, 31, 64, 0, 128, 6, 230, 22, 212, 25, 99, 74, 202, 177, 16, 121, 194, 156, 0, 119, 160, 128, 75, 200, 249, 141, 210, 78, 80, 24, 64, 252, 130, 182, 0, 0, 65, 82, 84, 73, 67, 76, 69, 32, 51, 52, 13, 10 };
+                var stream = new MemoryStream(arr, 0, arr.Length);
+                var reader = new BinaryReader(stream);
+
+                Console.WriteLine("Version and header length {0}", reader.ReadByte());
+                Console.WriteLine("Diff services{0}", reader.ReadByte());
+
+                Console.WriteLine("Total length {0}", IPAddress.NetworkToHostOrder(reader.ReadInt16()));
+                Console.WriteLine("ID {0}", IPAddress.NetworkToHostOrder(reader.ReadInt16()));
+                Console.WriteLine("Flags and offset {0}", IPAddress.NetworkToHostOrder(reader.ReadInt16()));
+
+                Console.WriteLine("TTL {0}", reader.ReadByte());
+                Console.WriteLine("Protocol {0}", reader.ReadByte());
+                Console.WriteLine("Checksum {0}", reader.ReadInt16());
+
+                Console.WriteLine("Source IP {0}", new IPAddress((int)reader.ReadInt32()));
+                Console.WriteLine("Destination IP {0}", new IPAddress((int)reader.ReadInt32()));
+
                 var programSimulator = new programPinSim();
                 var keypadSimulator  = new inputSimulator(programSimulator.keyControl);
 
