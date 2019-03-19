c6b7433ad2abdf26bcaec4d8165e76b1a3bb5ccf
PatrickSorensen <11074388+PatrickSorensen@users.noreply.github.com>
2019-03-19 14:55:22 +0100

    fixed
    push

--- simplePackageFilter.cs
+++ simplePackageFilter.cs
@@ -3,13 +3,29 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
+using System.Net;
 
 namespace simplePackageFilter
 {
 
     [TopLevelInputBus]
-    public interface IPv4_Simple : IBus {   
+    public interface IPv4_Simple : IBus
+    {
         byte Header { get; set; }
+
+        byte Diff { get; set; }
+
+        byte[] length { get; set; }
+
+        byte[] id { get; set; }
+
+        byte[] flags { get; set; }
+
+        byte ttl { get; set; }
+
+        byte protocol { get; set; }
+
+        byte[] checksum { get; set; }
 
         byte[] SourceIP { get; set; }
 
@@ -17,26 +33,50 @@ namespace simplePackageFilter
     }
 
     // Input class, which reads simple IPv4 bytes
-    public class inputSimulator : SimulationProcess {
+    public class inputSimulator : SimulationProcess
+    {
 
         [InputBus]
         public IPv4_Simple ipv4;
 
-        short[] sample;
+        byte[] sample;
 
-        public async override System.Threading.Tasks.Task Run() {
-            sample = File.ReadAllLines("ipv4_example.txt")
-                .Select(line => short.Parse(line))
-                .ToArray();
+        public async override System.Threading.Tasks.Task Run()
+        {
+            sample = File.ReadAllBytes("C:/Users/patri/OneDrive/Dokumenter/GitHub/FPGA_firewall/ipv4_bytes.txt");
+                
+            Console.WriteLine("hmm");
+            foreach (var item in sample)
+            {
+                Console.WriteLine(item);
+            }
 
-            foreach (short s in sample) {
+            byte[] arr = { 0x45, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x82, 0xe2, 0xed, 0xad, 0x5b, 0xe0, 0xd3, 0x47 };
+            var stream = new MemoryStream(sample, 0, sample.Length);
+            var reader = new BinaryReader(stream);
 
+
+            for (int i = 0; i < (sample.Length) / 80; i++)
+            {
                 // UPDATE IPv4_Simple INPUTBUS HERE
+                Console.WriteLine("Version and header length {0}", reader.ReadByte());
+                Console.WriteLine("Diff services {0}", reader.ReadByte());
 
-                await ClockAsync();
+                Console.WriteLine("Total length {0}", IPAddress.NetworkToHostOrder(reader.ReadInt16()));
+                Console.WriteLine("ID {0}", IPAddress.NetworkToHostOrder(reader.ReadInt16()));
+                Console.WriteLine("Flags and offset {0}", IPAddress.NetworkToHostOrder(reader.ReadInt16()));
+
+                Console.WriteLine("TTL {0}", reader.ReadByte());
+                Console.WriteLine("Protocol {0}", reader.ReadByte());
+                Console.WriteLine("Checksum {0}", reader.ReadInt16());
+
+                Console.WriteLine("Source IP {0}", new IPAddress(reader.ReadUInt32()));
+                Console.WriteLine("Destination IP {0}", new IPAddress(reader.ReadUInt32()));
+
             }
         }
     }
+
 
 
 
@@ -47,40 +87,17 @@ namespace simplePackageFilter
         {
             using (var sim = new Simulation())
             {
-//                sim
-//                  .BuildCSVFile()
-//                  .BuildVHDL();
+                sim
+                    .BuildCSVFile()
+                    .BuildVHDL();
+                //                var programSimulator = new programPinSim();
+                //                var keypadSimulator  = new inputSimulator(programSimulator.keyControl);
+                //
+                //                var control = new controlProcess(keypadSimulator.keyPressed, keypadSimulator.keyControl);
+                var some = new inputSimulator();
 
-                byte[] arr = { 0, 48, 136, 21, 69, 131, 0, 24, 231, 253, 174, 161, 8, 0, 69, 0, 0, 52, 2, 31, 64, 0, 128, 6, 230, 22, 212, 25, 99, 74, 202, 177, 16, 121, 194, 156, 0, 119, 160, 128, 75, 200, 249, 141, 210, 78, 80, 24, 64, 252, 130, 182, 0, 0, 65, 82, 84, 73, 67, 76, 69, 32, 51, 52, 13, 10 };
-                var stream = new MemoryStream(arr, 0, arr.Length);
-                var reader = new BinaryReader(stream);
-
-                Console.WriteLine("Version and header length {0}", reader.ReadByte());
-                Console.WriteLine("Diff services{0}", reader.ReadByte());
-
-                Console.WriteLine("Total length {0}", IPAddress.NetworkToHostOrder(reader.ReadInt16()));
-                Console.WriteLine("ID {0}", IPAddress.NetworkToHostOrder(reader.ReadInt16()));
-                Console.WriteLine("Flags and offset {0}", IPAddress.NetworkToHostOrder(reader.ReadInt16()));
-
-                Console.WriteLine("TTL {0}", reader.ReadByte());
-                Console.WriteLine("Protocol {0}", reader.ReadByte());
-                Console.WriteLine("Checksum {0}", reader.ReadInt16());
-
-                Console.WriteLine("Source IP {0}", new IPAddress((int)reader.ReadInt32()));
-                Console.WriteLine("Destination IP {0}", new IPAddress((int)reader.ReadInt32()));
-
-//                var programSimulator = new programPinSim();
-//                var keypadSimulator  = new inputSimulator(programSimulator.keyControl);
-//
-//                var control = new controlProcess(keypadSimulator.keyPressed, keypadSimulator.keyControl);
-
-//                sim.Run();
+                sim.Run();
             }
         }
     }
 }
-
-
-
-
-
