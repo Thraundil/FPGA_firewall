651774b7f94171cce112aab752d8cffe4e228d91
thraundil <emilsbak@gmail.com>
2019-04-12 16:30:10 +0200

    fredag

--- simplePackageFilter.cs
+++ simplePackageFilter.cs
@@ -25,6 +25,9 @@ namespace simplePackageFilter
     {
         [FixedArrayLength(2)]
         IFixedArray<bool> good_or_bad { get; set; }
+
+        [InitialValue(false)]
+        bool clockCheck { get; set; }
     }
 
     [TopLevelOutputBus]
@@ -33,35 +36,51 @@ namespace simplePackageFilter
         bool accept_or_deny { get; set; }
     }
 
+
+    // ****************************************************************************
     public class final_check : SimpleProcess
     {
         [InputBus]
-        public Itemp_name yes_or_no;
+        public Itemp_name procArray;
 
         [OutputBus]
         public Iprocess_bools final_say;
 
         public final_check(Itemp_name busIn)
         {
-            yes_or_no = busIn;
+            procArray = busIn;
         }
 
-        // Deicdes to throw away or keep the package
+        // Decides to throw away or keep the package
         private bool keep_track = false;
         protected override void OnTick()
         {
             keep_track = false;
-            for(int i = 0; i < yes_or_no.good_or_bad.Length; i++)
-            {
-                Console.WriteLine(yes_or_no.good_or_bad[i]);
-                if(yes_or_no.good_or_bad[i])
+            if (procArray.clockCheck) {
+                for(int i = 0; i < procArray.good_or_bad.Length; i++)
                 {
-                    keep_track = true;
-                    Console.WriteLine("TRUE DAT");
-                    break;
+    //                Console.WriteLine(procArray.good_or_bad[i]);
+                    if(procArray.good_or_bad[i])
+                    {
+                        Console.WriteLine(i);
+                        keep_track = true;
+                        Console.WriteLine("TRUE DAT");
+                    }
                 }
+//                procArray.clockCheck = false;
+
             }
         }
+    }
+
+
+    // ****************************************************************************
+    public class busExtra
+    {
+
+        [OutputBus]
+        public Itemp_name procArray = Scope.CreateBus<Itemp_name>();
+
     }
 
     // ****************************************************************************
@@ -71,7 +90,7 @@ namespace simplePackageFilter
         public IPv4_Simple ipv4;
 
         [OutputBus]
-        public Itemp_name TCP = Scope.CreateBus<Itemp_name>();
+        public Itemp_name procArray;
 
         // Int list[4] to compare IP Source/Destination
         private readonly int[] allowed_SourceIP = new int[4];
@@ -81,9 +100,10 @@ namespace simplePackageFilter
         private readonly int my_id = new int();
 
         // ipv4Reader_Constructor
-        public ipv4Reader(IPv4_Simple busIn, int[] SourceIP, int id)
+        public ipv4Reader(IPv4_Simple busIn1, Itemp_name busIn2, int[] SourceIP, int id)
         {
-            ipv4 = busIn;
+            ipv4 = busIn1;
+            procArray = busIn2;
             allowed_SourceIP = SourceIP;
             my_id = id;
             //allowed_ports = ports;
@@ -94,21 +114,23 @@ namespace simplePackageFilter
         {
             if (ipv4.SourceIP[0] == x[0])
             {
-                TCP.good_or_bad[my_id] = true;
+                procArray.good_or_bad[this.my_id] = true;
                 Console.WriteLine("The packet was accepted");
             }
             else
             {
-                TCP.good_or_bad[my_id] = false;
+                procArray.good_or_bad[this.my_id] = false;
                 Console.WriteLine("The packet was denied");
             }
+            if (my_id == 0) {
+                procArray.clockCheck = true;
+            }
         }
-
 
         // On Tick (ipv4Readers 'main')
         protected override void OnTick()
         {
-            TCP.good_or_bad[my_id] = false;
+            procArray.good_or_bad[this.my_id] = false;
             if (ipv4.clockCheck)
             {
                 sourceCompareIpv4(allowed_SourceIP);
@@ -134,18 +156,19 @@ namespace simplePackageFilter
                 var rules = new Rules();
                 var print = new Print();
                 var byte_input = new inputSimulator();
+                var busArrayProc = new busExtra();
 
                 // Prints a file, for testing purposes
                 // print.print_file(rules.accepted_sources);
-
                 // start one process for each rule
-                for (int i = 0; i < (rules.accepted_sources).Length; i++)
+                for (int i = 0; i < 2; i++)
                 {
-                    Console.WriteLine(i);
+//                    Console.WriteLine(i);
                     var ipv4Read = new ipv4Reader(byte_input.ipv4,
+                                                  busArrayProc.procArray,
                                                   rules.ip_str_to_int_array(rules.accepted_sources[i]),
                                                   i);
-                    var final_say = new final_check(ipv4Read.TCP);
+                    var final_say = new final_check(ipv4Read.procArray);
                 }
                 sim.Run();
             }
