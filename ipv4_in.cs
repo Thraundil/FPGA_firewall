b1397380f83d6f7e981b3cb59986b2cb9f5d8fc3
thraundil <emilsbak@gmail.com>
2019-04-30 11:44:02 +0200

    main

--- ipv4_in.cs
+++ ipv4_in.cs
@@ -149,54 +149,6 @@ namespace simplePackageFilter
             }
         }
     }
-
     // ****************************************************************************
-
-    // Main
-    public class Program
-    {
-        static void Main()
-        {
-
-            // General classes, compiled before simulation
-            var rules = new Rules();
-            _ = new Print();
-
-            // Number of rules, 
-            int len_sources = rules.accepted_sources.Length;
-            _ = rules.accepted_destinations.Length;
-
-            using (var sim = new Simulation())
-            {
-                sim
-                    .BuildCSVFile();
-                // CARL/KENNETH FIX PLZ
-                //                    .BuildVHDL();
-
-                // Creates 3 classes, each with their own uses
-                var byte_input = new InputSimulator();
-
-                // Bus array for each rule to write a bus to
-                
-                IBus_ruleVerdict[] Bus_array_sources = new IBus_ruleVerdict[len_sources];
-
-                // The bus loop, in which the above array is filled
-                for (int i = 0; i < len_sources; i++)
-                {
-                    var (low_src, high_src) = rules.Get_sources(i);
-                    var (low_dest, high_dest) = rules.Get_destination(i);
-                    var temptemp = new Rule_Process(byte_input.ipv4, low_src, high_src, low_dest, high_dest);
-                    Bus_array_sources[i] = temptemp.ruleVerdict;
-                }
-
-                var Final_verdict = new Final_check(Bus_array_sources);
-
-
-                // Prints a file, for testing purposes
-                // print.print_file(rules.accepted_sources);
-                sim.Run();
-            }
-        }
-    }
 }
 
